1729412be67c0c0b19effe53440fea179ae7c4df
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-01-28 17:52:57 +0900

    :construction_worker: Exit when mount process failed

--- scripts/tests/run.sh
+++ scripts/tests/run.sh
@@ -4,9 +4,18 @@ set -eu
 main() {
   pna create src.pna -r ./src --keep-dir --keep-permission --keep-timestamp --overwrite
   pna-fs mount src.pna ./mnt/pna/src/ &
+  PID=$(echo $!)
   while [ ! -e ./mnt/pna/src/src ]; do
     echo "Wait while mount ..."
     sleep 1
+    set +eu
+    ps -p "$PID" > /dev/null
+    EXIT_CODE=$?
+    set -eu
+    if [ $EXIT_CODE -ne 0 ]; then
+      echo "mount process failed"
+      break
+    fi
   done
   echo "Checking files ..."
   diff -r ./src ./mnt/pna/src/src
