891bf06f58853e4885a228d5aad427d92ad93a48
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-02-05 23:38:38 +0900

    :alembic: Change option to subcommand

--- src/command.rs
+++ src/command.rs
@@ -1,23 +1,15 @@
+#[cfg(feature = "unstable-generate")]
+pub(crate) mod generate;
 pub(crate) mod mount;
 
 use crate::cli::{Cli, PasswordArgs, SubCommand};
-#[cfg(feature = "unstable-generate")]
-use clap::CommandFactory;
-#[cfg(feature = "unstable-generate")]
-use clap_complete::{generate, Generator};
 use std::io;
-#[cfg(feature = "unstable-generate")]
-use std::{env, path::PathBuf};
 
 pub(crate) fn entry(args: Cli) -> io::Result<()> {
-    #[cfg(feature = "unstable-generate")]
-    if let Some(shell) = args.generate {
-        print_completions(shell, &mut Cli::command());
-        return Ok(());
-    }
     match args.subcommand {
-        Some(SubCommand::Mount(args)) => args.execute(),
-        None => Ok(()),
+        SubCommand::Mount(args) => args.execute(),
+        #[cfg(feature = "unstable-generate")]
+        SubCommand::Generate(args) => args.execute(),
     }
 }
 
@@ -34,15 +26,4 @@ fn ask_password(args: PasswordArgs) -> io::Result<Option<String>> {
         Some(None) => Some(rpassword::prompt_password("Enter password: ")?),
         None => None,
     })
-}
-
-#[cfg(feature = "unstable-generate")]
-fn print_completions<G: Generator>(gen: G, cmd: &mut clap::Command) {
-    let name = env::args().next().map(PathBuf::from).unwrap();
-    generate(
-        gen,
-        cmd,
-        name.file_name().unwrap().to_string_lossy(),
-        &mut io::stdout(),
-    );
 }
